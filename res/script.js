458e41bb4339ed5a7da6776a4de4034ea6b83a7e
vsateoli <venanciosateoli@gmail.com>
2019-11-25 07:29:56 -0300

    Inserindo animais na pilha

--- res/script.js
+++ res/script.js
@@ -1,16 +1,28 @@
 var animais = ["gato","onca","koala","macaco","urso","panda", "vazio"];
 var posicoes = ["vazio"];
+const vazio = "vazio";
+
 $( document ).ready(function() {
-sortBubbles();
-colocarAnimalParaLancamento();
+  sortBubbles();
+  colocarAnimalParaLancamento();
+  $("#jogo").click(function(event){
+      var animalEmbaixo = $(".image")[0].classList[2];
+      alocaAnimal(animalEmbaixo,event.offsetX,event.offsetY);
+  });
+  // $("#jogo").mousemove(function (event){
+  //   escreverLinha(event.offsetX,event.offsetY)
+  // })
 });
 
 function sortBubbles(){
   var cont = 0;
   console.log('Sorteando Animais!');
-  for(var i = 0; i< 55; i++){
+  for(var i = 0; i< 44; i++){
     var randomico = Math.floor(Math.random()*animais.length);
     posicoes[i] = animais[randomico];
+  }
+  for(i = 44 ; i<=55; i++){
+    posicoes[i] = "vazio";
   }
   for (i = 0; i < 5; i++){
     for (var j = 0; j< 11; j++){
@@ -20,8 +32,11 @@ function sortBubbles(){
   }
 }
 function colocarAnimalParaLancamento(){
-  var randomico = Math.floor(Math.random()*animais.length-1);
+  var randomico = Math.floor(Math.random()*(animais.length-1));
+  if (randomico == 0) randomico = 1;
   console.log("randomico para lançamento = " + animais[randomico]);
+  //trocar para replace normal
+  $("#areaLancamento").children()[0].classList.replace($("#areaLancamento").children()[0].classList[2],animais[randomico])
   $("#areaLancamento").html($("#areaLancamento").html().replace("vazio",animais[randomico]));
 }
 
@@ -90,4 +105,54 @@ function verificaAbaixo(posicao,elemento){
     $(".p"+(posicao+11))[0].classList.replace(elemento,"vazio");
     verificarColisao(posicao-1, elemento);
   }
+}
+
+function recuperaIndice(x,y){
+  indice = 0;
+  //EIXO X 
+  for(var i = 1; i<11; i++){
+    if (x >= 0 && x <= 70){
+      indice = 1;
+      break;
+    }
+    if(x > i * 70 && x <= (i +1) * 70){
+      indice = i+1;
+      break;
+    }
+  }
+  // EIXO Y
+  for(var i = 1; i<10; i++){
+    if (y >= 0 && y <= 70){
+      break;
+    }
+    if(y > i * 70 && y <= (i +1) * 70){
+      indice = (i* 11) + indice;
+      break;
+    }
+  }
+  return indice;
+}
+
+function alocaAnimal(animal, x,y){
+  var indice = recuperaIndice(x,y);
+  var animalPosicao = $("#jogo .p"+indice)[0].classList[1];
+  if (animalPosicao == vazio){
+    $("#jogo .p"+indice)[0].classList.replace("vazio",animal);
+    var interval = setInterval(() => {
+      verificarColisao(indice);
+      colocarAnimalParaLancamento();
+      clearInterval(interval);
+    }, 500);
+    
+  }
+}
+function escreverLinha(x,y){
+  $('canvas').clearCanvas();
+  $('canvas').drawLine({
+    strokeStyle: '#000',
+    rounded: true,
+    strokeWidth: 2,
+    x1: x, y1: 110,
+    x2: 150, y2: 0
+  });
 }
